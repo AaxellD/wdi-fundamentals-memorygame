266908bb399bceb8d804066c5b6b0d35a674dc4f
Xander Dean <aaxelld@gmail.com>
2020-01-26 00:34:03 -0500

    Functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,18 +3,10 @@ console.log("Up and running!");
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-var test = (x) => {
-console.log("User flipped " + x);
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found a match!");
-    } else {
-        alert("Sorry, try again!");
-    }
+function flipCard (cardId) {
+    console.log("User has flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
 }
 
-test(cardTwo);
+flipCard(1);
+
