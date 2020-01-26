f3399d1b3e6b9363b4c272d1b11906d34460e735
Xander Dean <aaxelld@gmail.com>
2020-01-26 14:54:24 -0500

    Taylor is a hottiegit commit -m git commit -m !

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,10 +3,26 @@ console.log("Up and running!");
 const cards = ["queen","queen","king","king"];
 const cardsInPlay = [];
 
+function checkForMatch () {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        console.log("You found a match!");
+      } else {
+        console.log("Sorry, try again.");
+      }
+}
+
 function flipCard (cardId) {
     console.log("User has flipped " + cards[cardId]);
     cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+
 }
 
 flipCard(1);
 
+var taylor = {
+    sexy:true,
+    age:22
+    eyes:brown,
+    hairColor:brown
+}
