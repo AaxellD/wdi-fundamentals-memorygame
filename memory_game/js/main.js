fbdf55afc3b61c56414c25a3cb1711c749fd7550
Xander Dean <aaxelld@gmail.com>
2020-01-27 05:14:11 -0500

    DOM applied and game finished

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,7 @@
+/// Lets the us know the script is working.
 console.log("Up and running!");
 
+// Object to hold the cards
 const cards = [
     {
         rank:"queen",
@@ -23,23 +25,45 @@ const cards = [
     }
 ];
 
-
+/// Cards that are in play at the moment
 const cardsInPlay = [];
 
+// Comparison to check for winning condition
 function checkForMatch () {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
+        //// HAVING TROUBLE HERE??????????? I am not refrencing  //
+
+        if (cardsInPlay[0] === cardsInPlay[1]) {
         console.log("You found a match!");
       } else {
         console.log("Sorry, try again.");
       }
 }
 
-function flipCard (cardId) {
+// function to flip the cards
+function flipCard () {
+    var cardId = this.getAttribute('data-id');
     console.log("User has flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage)
-    checkForMatch();
+    this.setAttribute('src',cards[cardId].cardImage);
+    //check for 2 cards
+    if (cardsInPlay.length>1) {
+        checkForMatch();
+        cardsInPlay.pop();
+        cardsInPlay.pop();
+    }
+    
 }
 
-flipCard(0);
-flipCard(2);
+/// Creates game-board and populates with cards
+function createBoard () {
+    for (let i=0 ; i<cards.length ; i++) {
+        let cardElement = document.createElement('img');
+        cardElement.setAttribute('src','images/back.png');
+        cardElement.setAttribute('data-id',i);
+        cardElement.addEventListener('click',flipCard);
+        document.querySelector('#game-board').appendChild(cardElement);
+    }
+}
+/// Invoce the Game
+createBoard();
